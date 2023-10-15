345fd683301814bd83b8c1940091bd7c6719896a
phuong le <phuongleminh841@gmail.com>
2023-10-15 19:45:12 +0700

    base line for html file

---
+++ script.js
@@ -0,0 +1,19 @@
+let variableA, variableB, operator;
+
+function add(variableA, variableB) {
+  return variableA + variableB;
+}
+function subtract(variableA, variableB) {
+  return variableA - variableB;
+}
+function multiply(variableA, variableB) {
+  return variableA * variableB;
+}
+function divide(variableA, variableB) {
+  return variableA / variableB;
+}
+function operate(variableA,variableB,operator) {
+    add(variableA,variableB)
+}
+
+console.log(divide(2, 3));
