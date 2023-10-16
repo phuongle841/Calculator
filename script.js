4ac6c17434b01b818d02a902ddfb144cc2615d30
phuong le <phuongleminh841@gmail.com>
2023-10-16 22:13:51 +0700

    add function to output string

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
+let numberButtons = document.querySelectorAll(".button.number");
+let upperLine = document.querySelector(".upperLine");
+console.log();
+
+let state = 1;
+// state 1: variableA
+// state 2: operator
+// state 3: variableB
+// state 4: Ans -> state1
+//  Ans -> state2
+let upperLineStack = {
+  variableA: 0,
+  operator: "+",
+  variableB: 0,
+};
+let lowerLineStack = {
+  Ans: "0",
+};
 let variableA, variableB, operator;
 
-function add(variableA, variableB) {
-  return variableA + variableB;
+let operatorClass = {
+  add: function (variableA, variableB) {
+    return variableA + variableB;
+  },
+  subtract: function (variableA, variableB) {
+    return variableA - variableB;
+  },
+  multiply: function (variableA, variableB) {
+    return variableA * variableB;
+  },
+  divide: function (variableA, variableB) {
+    return variableA / variableB;
+  },
+};
+function operate(variableA, variableB, operator) {
+  return operatorClass[operator](variableA, variableB);
 }
-function subtract(variableA, variableB) {
-  return variableA - variableB;
+let test = operate(3, 7, "add");
+console.log(test);
+
+function SetUpperLine(string) {
+  upperLine.textContent = string;
 }
-function multiply(variableA, variableB) {
-  return variableA * variableB;
+function addDecimal(variableA, variableB) {
+  console.log(variableA, variableB);
+  return variableA * 10 + variableB;
 }
-function divide(variableA, variableB) {
-  return variableA / variableB;
-}
-function operate(variableA,variableB,operator) {
-    add(variableA,variableB)
+function stringUpperLine(upperLineStack) {
+  let result = `${upperLineStack.variableA} ${upperLineStack.operator} ${upperLineStack.variableB}`;
+  return result;
 }
 
-console.log(divide(2, 3));
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", function () {
+    let currentVariable = upperLineStack.variableA;
+    let variable = Number(numberButton.dataset.number);
+    let output = addDecimal(currentVariable, variable);
+    upperLineStack.variableA = output;
+    SetUpperLine(output);
+  });
+});
