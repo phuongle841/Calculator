59eb7959fdc64364de275d9c8dd3a3c55d7a616e
phuong le <phuongleminh841@gmail.com>
2023-10-22 14:44:53 +0700

    finish the draft of the structure things like input, output and the visualize.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 let numberButtons = document.querySelectorAll(".button.number");
+let functionButtons = document.querySelectorAll(".button.operator");
 let upperLine = document.querySelector(".upperLine");
-console.log();
+let lowerLine = document.querySelector(".lowerLine");
+
+
+let max = Number.MAX_SAFE_INTEGER;
+let min = Number.MIN_SAFE_INTEGER;
 
 let state = 1;
 // state 1: variableA
 // state 2: operator
 // state 3: variableB
 // state 4: Ans -> state1
-//  Ans -> state2
+//          Ans -> state2
+
+// C => reset state
+// "=" => state 4
 let upperLineStack = {
-  variableA: 0,
-  operator: "+",
-  variableB: 0,
+  variableA: "",
+  operator: "",
+  variableB: "",
 };
 let lowerLineStack = {
-  Ans: "0",
+  Ans: "",
 };
 let variableA, variableB, operator;
 
@@ -31,31 +39,67 @@ let operatorClass = {
   divide: function (variableA, variableB) {
     return variableA / variableB;
   },
+  operate: function (variableA, variableB, operator) {
+    return operatorClass[operator](variableA, variableB);
+  },
 };
-function operate(variableA, variableB, operator) {
-  return operatorClass[operator](variableA, variableB);
-}
-let test = operate(3, 7, "add");
-console.log(test);
 
-function SetUpperLine(string) {
-  upperLine.textContent = string;
-}
 function addDecimal(variableA, variableB) {
-  console.log(variableA, variableB);
-  return variableA * 10 + variableB;
+  let variable = variableA * 10 + +variableB;
+  console.log(variable);
+  return variable;
 }
-function stringUpperLine(upperLineStack) {
+function refreshUpperLine() {
   let result = `${upperLineStack.variableA} ${upperLineStack.operator} ${upperLineStack.variableB}`;
-  return result;
+  upperLine.textContent = result;
 }
+function refreshLowerLine() {
+  let result = `${upperLineStack.result}`;
+  upperLine.textContent = result;
+}
+function state1(numberButton) {
+  let currentVariable = upperLineStack.variableA;
+  let variable = Number(numberButton.dataset.number);
+  let output = addDecimal(currentVariable, variable);
+  upperLineStack.variableA = output;
+  refreshUpperLine();
+}
+function state2(functionButton) {
+  let operate = functionButton.dataset.function;
+  let operator = functionButton.textContent;
+  if (operate in operatorClass) {
+    upperLineStack.operator = operator;
+    refreshUpperLine();
+  }
+}
+function state3(numberButton) {
+  let currentVariable = upperLineStack.variableB;
+  let variable = Number(numberButton.dataset.number);
+  let output = addDecimal(currentVariable, variable);
+  upperLineStack.variableB = output;
+  refreshUpperLine();
+}
+
+functionButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (state == 1 || state == 2) {
+      state2(this);
+      state = 3;
+    }
+  });
+});
 
 numberButtons.forEach((numberButton) => {
   numberButton.addEventListener("click", function () {
-    let currentVariable = upperLineStack.variableA;
-    let variable = Number(numberButton.dataset.number);
-    let output = addDecimal(currentVariable, variable);
-    upperLineStack.variableA = output;
-    SetUpperLine(output);
+    if (state == 1) {
+      state1(this);
+    }
+    if (state == 3) {
+      state3(this);
+    }
   });
 });
+
+function setup() {
+  //
+}
